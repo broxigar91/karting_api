d3de5366be89117b739ef70821705f2d47d9cd32
Ricardo Valls Luna <ichigobiar@gmail.com>
2021-02-21 13:06:10 +0100

    Data loading
    Fixture to load data at the begining.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,69 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Pilot;
+use App\Entity\PilotRaceLap;
+use App\Entity\Race;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+               
         // $product = new Product();
         // $manager->persist($product);
 
+        $fileData = file_get_contents("src/Resources/drivers_karts_BackJ.json");
+        $karting = new ArrayCollection(json_decode($fileData));
+
+        
+        $races = new ArrayCollection($karting[0]->races);
+        $races= $races->map(function($v){
+            return $v->name;
+        })->toArray();
+
+        foreach($races as $i => $r)
+        {
+            $race = new Race();
+            $race->setId($i+1);
+            $race->setName($r);
+            $manager->persist($race);
+        }
+        
         $manager->flush();
+                      
+        foreach($karting as $k)
+        {
+            $pilot = new Pilot();
+            $pilot->setId($k->_id);
+            $pilot->setPicture($k->picture);
+            $pilot->setAge($k->age);
+            $pilot->setName($k->name);
+            $pilot->setTeam($k->team);
+
+            $manager->persist($pilot);
+
+            $races = new ArrayCollection($k->races);
+
+            foreach($races as $i => $r)
+            {
+                foreach(new ArrayCollection($r->laps) as $lap)
+                {
+                    $pilotRaceLap = new PilotRaceLap();
+                    $pilotRaceLap->setPilot($pilot);
+                    $pilotRaceLap->setRace($manager->find(Race::class,$i+1));
+                    $pilotRaceLap->setTime($lap->time);
+
+                    $manager->persist($pilotRaceLap);
+                }
+            }
+            
+        }
+
+        $manager->flush();
+
     }
 }
